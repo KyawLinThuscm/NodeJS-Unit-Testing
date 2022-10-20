89379f2d6475226889a7a9121db7fe324d04491e
KyawLinThuscm <scm.kyawlinthu@gmail.com>
2022-10-20 16:49:54 +0630

    service change

--- src/services/product.service.js
+++ src/services/product.service.js
@@ -4,6 +4,6 @@ export async function createProduct(input) {
     return ProductModel.create(input);
 }
 
-export async function findProduct(query,options) {
-    return ProductModel.findOne(query, {}, options);
+export async function findProduct(query) {
+    return ProductModel.findOne(query);
 }
